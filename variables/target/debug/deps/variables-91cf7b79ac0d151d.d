35c3a90a717b10d7cc255628b0e7f43b1c2bde35
Shrey <shreyk@vt.edu>
2025-11-24 22:22:55 -0600

    adding variables

---
+++ variables/target/debug/deps/variables-91cf7b79ac0d151d.d
@@ -0,0 +1,5 @@
+C:\Users\shrey\Desktop\Post Grad Development\Rust\variables\target\debug\deps\variables-91cf7b79ac0d151d.d: src\main.rs
+
+C:\Users\shrey\Desktop\Post Grad Development\Rust\variables\target\debug\deps\libvariables-91cf7b79ac0d151d.rmeta: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/deps/variables-d58a71928a6d438f.d
@@ -0,0 +1,5 @@
+C:\Users\shrey\Desktop\Post Grad Development\Rust\variables\target\debug\deps\variables-d58a71928a6d438f.d: src\main.rs
+
+C:\Users\shrey\Desktop\Post Grad Development\Rust\variables\target\debug\deps\libvariables-d58a71928a6d438f.rmeta: src\main.rs
+
+src\main.rs:
