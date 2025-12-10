67d881329554e986eb966840caa355fec583a42f
Shrey <shreyk@vt.edu>
2025-12-09 22:05:58 -0600

    making updates

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -10,6 +10,23 @@ fn main() {
     x = x + 1; 
     println!("x is {}", x);
 
-    let x: f32 = 10.09;
+    let x: f32 = 10.2132132132109; //floats
     println!("x is {}", x);
+
+    let x: f64 = 10.2132132132109; //floats
+    println!("x is {}", x);
+
+
+    // arithmetic operations 
+    let a = 10;
+    let b = 3.3;
+    // can't divide two different data types so casting is used
+    println!("a/b is: {}", a as f64 /b); 
+
+    //print statements
+    print!("a/b is: {:08.3}\na is {}", a as f64 /b, a as f64); 
+    println!("a/b is: {:08.3}\na is {}", a as f64 /b, a as f64); 
+    println!("a is {0} \nb is {1}\na again is {0}", a, b);
+
+
 }
